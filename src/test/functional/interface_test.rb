4a24f2ad2c71f9ac612f08b12dcfd3dfab473f98
Mohammed Morsi <mmorsi@redhat.com>
2008-11-12 12:11:51 -0500

    various small fixes / improvements to selenium interface tests
     enumerated tests to ensure execution order
     added test_create_resource_pool
     fix test_create_start_stop_vm

--- src/test/functional/interface_test.rb
+++ src/test/functional/interface_test.rb
@@ -44,11 +44,11 @@ if File.exists? File.dirname(__FILE__) + '/../selenium.rb'
         @browser.stop
      end
 
-     def test_ovirt
+     def test_1_ovirt
         assert_equal("Dashboard", @browser.get_title())
      end
 
-     def test_view_pool
+     def test_2_view_pool
       @browser.open("http://192.168.50.2/ovirt/")
       @browser.wait_for_condition(
             "selenium.isElementPresent(\"//ul[@id='nav_tree']/li/div\")",
@@ -64,14 +64,46 @@ if File.exists? File.dirname(__FILE__) + '/../selenium.rb'
                   @browser.get_text("//div[@class='summary_title']"))
      end
 
-     def test_create_start_stop_vm
-        pool_link="//ul[@id='nav_tree']/li/ul/li[1]/div"
-        @browser.open("http://192.168.50.2/ovirt/")
+     def test_3_create_resource_pool
+        # get us to default pool:
+        test_2_view_pool
+
+        # click on virtual machines pools tab
+        @browser.click "//li[@id='nav_vmpool']/a"
         @browser.wait_for_condition(
-              "selenium.isElementPresent(\""+ pool_link +"\")",
+               "selenium.isElementPresent(\"//div[@id='toolbar_nav']/ul/li[1]/a\")",
                10000)
-        # click on 'foobar' vm resource pool  link:
-        @browser.click pool_link
+        # click on 'new virtual machine pool'
+        @browser.click "//div[@id='toolbar_nav']/ul/li[1]/a"
+
+        # fill in required fields
+        @browser.wait_for_condition(
+               "selenium.isElementPresent(\"//input[@id='vm_resource_pool_name']\")",
+               10000)
+        @browser.type("//input[@id='vm_resource_pool_name']", "vm_resource_pool1")
+        # click the button
+        @browser.click "//form[@id='vm_pool_form']/div[2]/div[2]/div[2]/a"
+
+        @pool_link="//ul[@id='nav_tree']/li/ul/li/div"
+        @browser.wait_for_condition(
+              "selenium.isElementPresent(\""+ @pool_link +"\")",
+               10000)
+     end
+
+     def test_4_create_start_stop_vm
+        # view default pool's subpools
+        @browser.wait_for_condition(
+              "selenium.isElementPresent(\"//ul[@id='nav_tree']/li/span\")",
+               10000)
+        @browser.click(
+            "//ul[@id='nav_tree']/li/span")  # click expand sublist checkbox
+
+        # click on 'vm_resource_pool1' vm resource pool  link:
+        @pool_link="//ul[@id='nav_tree']/li/ul/li/div"
+        @browser.wait_for_condition(
+              "selenium.isElementPresent(\""+ @pool_link +"\")",
+               10000)
+        @browser.click @pool_link
         @browser.wait_for_condition(
              "selenium.isElementPresent(\"//li[@id='nav_vmpool']/a\")",
               10000)
@@ -116,7 +148,7 @@ if File.exists? File.dirname(__FILE__) + '/../selenium.rb'
               10000)
         @browser.click "//div[@id='vm_action_results']/div[3]/div/div[2]/a"
         sleep 5 # give vm time to start
-        @browser.click pool_link
+        @browser.click @pool_link
         @browser.wait_for_condition(
              "selenium.isElementPresent(\"//li[@id='nav_vmpool']/a\")",
               10000)
@@ -137,7 +169,7 @@ if File.exists? File.dirname(__FILE__) + '/../selenium.rb'
               10000)
         @browser.click "//div[@id='vm_action_results']/div[3]/div/div[2]/a"
         sleep 5 # give vm time to stop
-        @browser.click pool_link
+        @browser.click @pool_link
         @browser.wait_for_condition(
              "selenium.isElementPresent(\"//table[@id='vms_grid']/tbody/tr[" + (num_vms.to_i + 1).to_s + "]/td[7]/div\") && " +
              "selenium.getText(\"//table[@id='vms_grid']/tbody/tr[" + (num_vms.to_i + 1).to_s + "]/td[7]/div\") == \"stopped\"",
