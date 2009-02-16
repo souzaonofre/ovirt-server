40ca57ab26f8f21f7e053ac01471977d94df3523
Joey Boggs <jboggs@redhat.com>
2009-02-16 13:05:49 -0500

    removing last dnsmasq patch, will be replaced with managed connections in amqp

--- installer/modules/ovirt/manifests/dns.pp
+++ installer/modules/ovirt/manifests/dns.pp
@@ -65,11 +65,6 @@ define dns::common($mgmt_ipaddr="", $prov_ipaddr="",$mgmt_dev="",$prov_dev="") {
         require => [Single_exec["set_hostname"], Package["dnsmasq"], File["/etc/dhclient.conf"]]  ,
         notify => Service[dnsmasq],
     }
-
-    single_exec {"make_dnsmasq_start_earlier":
-        command => "/bin/mv /etc/rc3.d/S99dnsmasq /etc/rc3.d/S95dnsmasq;/bin/mv /etc/rc5.d/S99dnsmasq /etc/rc5.d/S95dnsmasq",
-        require => Package["dnsmasq"]
-    }
 }
 
 define dns::bundled($mgmt_ipaddr="", $prov_ipaddr="",$mgmt_dev="",$prov_dev="") {
