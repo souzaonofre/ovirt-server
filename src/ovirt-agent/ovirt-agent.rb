c515214b0bb7d3651ad773cf0605c07889aba34b
David Lutterkort <lutter@redhat.com>
2009-05-22 11:24:59 -0700

    * ovirt-agent.rb: reorganize
    Use schema parser to set up classes, and organize controllers etc. a little
    differently.

--- src/ovirt-agent/ovirt-agent.rb
+++ src/ovirt-agent/ovirt-agent.rb
@@ -1,6 +1,9 @@
 #!/usr/bin/ruby
 
 $: << File.join(File.dirname(__FILE__), "../dutils")
+$: << File.join(File.dirname(__FILE__), "lib")
+
+SCHEMA_XML = File.join(File.dirname(__FILE__), "ovirt_api.xml")
 
 require "rubygems"
 require 'monitor'
@@ -11,120 +14,149 @@ require 'logger'
 require 'qmf'
 require 'socket'
 
+require 'ovirt'
+
 include Daemonize
 
-class VmQmfController
-  include VmService
+# Monkey patch
+class Qmf::SchemaObjectClass
+  attr_reader :id
 
-  attr_accessor :vm
+  def id
+    name.hash & 0xffff
+  end
 
+  def name
+    impl.getName
+  end
+end
+
+class Qmf::Agent
+
+  # We use object_num_high (32 bit int) to denote the controller class
+  # and object_num_low (32 bits also) to denote the row ID in the database.
+
+  def controller_id(object_id)
+    object_id.object_num_high
+  end
+
+  def row_id(object_id)
+    object_id.object_num_low
+  end
+
+  def decode_id(object_id)
+    [ controller_id(object_id), row_id(object_id) ]
+  end
+
+  def encode_id(controller_id, row_id)
+    alloc_object_id(row_id, controller_id)
+  end
+end
+
+class ControllerNotFoundError < StandardError ; end
+
+# The base class for all QMF controllers
+#
+# Right after the agent is initialized, the +schema_class+ class attribute
+# is set, and the +setup+ class method is called with the agent as its only
+# argument
+class AgentController
+  class_inheritable_accessor :schema_class
+
+  attr_reader :agent, :logger
+  attr_accessor :args
+
+  def initialize(context, agent, logger)
+    @context = context
+    @agent = agent
+    @logger = logger
+    @args = {}
+  end
+
+  def schema_class
+    self.class.schema_class
+  end
+
+  def encode_id(row_id)
+    @agent.encode_id(schema_class.id, row_id)
+  end
+
+  # FIXME: Get the actual user from the QMF session
   def get_login_user
     return "ovirtadmin"
   end
+
+  # Subclasses should have
+  #
+  #   find(id) : look up object with that id and return the QMF object
+  #   list     : return list of all QMF objects
+  #
+  #   methods with the same name as defined in the schema
+  #     arguments can be accessed through method 'args'
 end
 
+class OvirtController < AgentController
 
-class Ovirt
+  include VmService
 
-  TABLE_ID = 1
-
-  def initialize(agent, logger)
-
-    @agent = agent
-    @logger = logger
-
-    @ovirt_class = Qmf::SchemaObjectClass.new("org.ovirt.ovirt", "Ovirt")
-    @ovirt_class.add_property(Qmf::SchemaProperty.new("version", Qmf::TYPE_SSTR, :access => Qmf::ACCESS_READ_CREATE, :desc => "Ovirt version string"))
-
-    method = Qmf::SchemaMethod.new("create_vm_def", :desc => "Define a new virtual machine definition.")
-    method.add_argument(Qmf::SchemaArgument.new("description", Qmf::TYPE_LSTR, :desc => "Description of new VM definition"))
-    method.add_argument(Qmf::SchemaArgument.new("num_vcpus_allocated", Qmf::TYPE_UINT32, :desc => "Number of virtual CPUs to allocate."))
-    method.add_argument(Qmf::SchemaArgument.new("memory_allocated", Qmf::TYPE_UINT64, :desc => "Amount of memory to allocate.", :units => "KB"))
-    method.add_argument(Qmf::SchemaArgument.new("uuid", Qmf::TYPE_SSTR, :desc => "UUID of VM, will be assigned if left empty."))
-    method.add_argument(Qmf::SchemaArgument.new("vnic_mac_addr", Qmf::TYPE_SSTR, :desc => "MAC address of virtual NIC, will be assigned if left empty."))
-    method.add_argument(Qmf::SchemaArgument.new("vm", Qmf::TYPE_REF, :desc => "Newly created domain object id.", :dir => Qmf::DIR_OUT))
-    @ovirt_class.add_method(method)
-
-    @agent.register_class(@ovirt_class)
+  # Special gymnastics since this is a singleton class
+  def self.setup(agent)
+    @@instance = Qmf::QmfObject.new(schema_class)
+    @@instance[:version] = "0.0.0.1"
+    obj_id = agent.encode_id(schema_class.id, 1)
+    @@instance.set_object_id(obj_id)
+    puts "Setup singleton for #{schema_class.name}: #{@@instance.inspect}"
   end
 
-  def start
-    @ovirt = Qmf::QmfObject.new(@ovirt_class)
-    @ovirt.set_attr("version", "0.0.0.1")
-
-    obj_id = @agent.alloc_object_id(1, Ovirt::TABLE_ID)
-    @ovirt.set_object_id(obj_id)
+  def self.instance
+    @@instance
   end
 
-  def implement_id_query(context, id)
+  def find(id)
     if id == 1
       # This class is a singleton so it's easy.. :)
-      @agent.query_response(context, @ovirt)
+      @@instance
     end
   end
 
-  def implement_class_query(context)
+  def list
     puts "query for 'Ovirt' object"
-    @agent.query_response(context, @ovirt)
+    [ @@instance ]
   end
 
-  def implement_method_call(context, name, object_num_low, args)
-    case name
-    when 'create_vm_def'
-      # args is a Qmf::Arguments and is not a real hash even though it implements [], each etc.
-      vm_hash = {}
-      args.each do |key, value|
-        puts "key is #{key}, value is #{args[key]}"
-        vm_hash[key] = value
-      end
-      vm_hash.delete('vm')
-      vm_hash.delete('uuid') if args['uuid'] == ''
-      vm_hash.delete('vnic_mac_addr') if args['vnic_mac_addr'] == ''
-
-      # FIXME: DOH!  Need to implement pools..
-      vm_hash['vm_resource_pool_id'] = 5
-      # FIXME: This needs to come from the service layer too..
-      vm_hash['boot_device'] = '/dev/sda1'
-      # FIXME: Scott has made a patch to have these created in the service layer if
-      # they are not provided.  Saves duplicating code here.
-      vm_hash['uuid'] = '23a4255f-1f0f-c5d2-5f8e-388537fde0b1' if args['uuid'] == ''
-      vm_hash['vnic_mac_addr'] = 'AB:CD:EF:00:00:00' if args['vnic_mac_addr'] == ''
-
-      vmsvc = VmQmfController.new
-      vmsvc.svc_create(vm_hash, false)
-
-      # Set our DIR_OUT argument to the object id of our new vm.
-      args['vm'] = @agent.alloc_object_id(vmsvc.vm.id, VmDef::TABLE_ID)
-      @agent.method_response(context, 0, "OK", args)
+  def create_vm_def
+    vm_hash = {}
+    args.each do |key, value|
+      puts "key is #{key}, value is #{args[key]}"
+      vm_hash[key] = value
     end
+    vm_hash.delete('vm')
+    vm_hash.delete('uuid') if args['uuid'] == ''
+    vm_hash.delete('vnic_mac_addr') if args['vnic_mac_addr'] == ''
+
+    # FIXME: DOH!  Need to implement pools..
+    vm_hash['vm_resource_pool_id'] = 5
+    # FIXME: This needs to come from the service layer too..
+    vm_hash['boot_device'] = '/dev/sda1'
+    # FIXME: Scott has made a patch to have these created in the service layer if
+    # they are not provided.  Saves duplicating code here.
+    vm_hash['uuid'] = '23a4255f-1f0f-c5d2-5f8e-388537fde0b1' if args['uuid'] == ''
+    vm_hash['vnic_mac_addr'] = 'AB:CD:EF:00:00:00' if args['vnic_mac_addr'] == ''
+
+    svc_create(vm_hash, false)
+
+    # Set our DIR_OUT argument to the object id of our new vm.
+    args['vm'] = encode_id(@vm.id)
   end
 end
 
-class VmDef
+class VmDefController < AgentController
 
-  TABLE_ID = 2
-
-  def initialize(agent, logger)
-
-    @agent = agent
-    @logger = logger
-
-    @vmdef_class = Qmf::SchemaObjectClass.new("org.ovirt.ovirt", "VmDef")
-    @vmdef_class.add_property(Qmf::SchemaProperty.new("description", Qmf::TYPE_LSTR, :desc => "VM description/name."))
-    @vmdef_class.add_property(Qmf::SchemaProperty.new("num_vcpus_allocated", Qmf::TYPE_UINT32, :desc => "Number of virtual CPUs to allocate to VM."))
-    @vmdef_class.add_property(Qmf::SchemaProperty.new("memory_allocated", Qmf::TYPE_UINT64, :units => 'KB', :desc => "Amount of memory to allocate to VM."))
-    @vmdef_class.add_property(Qmf::SchemaProperty.new("mac", Qmf::TYPE_SSTR, :access => Qmf::ACCESS_READ_CREATE, :desc => "VM virtual network card MAC address."))
-    @vmdef_class.add_property(Qmf::SchemaProperty.new("uuid", Qmf::TYPE_SSTR, :access => Qmf::ACCESS_READ_CREATE, :desc => "VM description/name."))
-    @vmdef_class.add_property(Qmf::SchemaProperty.new("provisioning", Qmf::TYPE_LSTR, :access => Qmf::ACCESS_READ_WRITE, :desc => "VM description/name."))
-    @vmdef_class.add_property(Qmf::SchemaProperty.new("needs_restart", Qmf::TYPE_BOOL, :access => Qmf::ACCESS_READ_ONLY, :desc => "VM description/name."))
-    @vmdef_class.add_property(Qmf::SchemaProperty.new("state", Qmf::TYPE_SSTR, :access => Qmf::ACCESS_READ_ONLY, :desc => "Current state of VM instance."))
-
-    @agent.register_class(@vmdef_class)
-  end
+  include VmService
 
   def ar_to_object(ar_vm)
 
-    vmdef = Qmf::QmfObject.new(@vmdef_class)
+    vmdef = Qmf::QmfObject.new(schema_class)
     vmdef.set_attr("description", ar_vm.description)
     vmdef.set_attr("num_vcpus_allocated", ar_vm.num_vcpus_allocated)
     vmdef.set_attr("memory_allocated", ar_vm.memory_allocated)
@@ -137,47 +169,35 @@ class VmDef
     # Set the 'low' part of the local object ID to the VM row id, and the
     # 'high' part of the local object id to the table ID.  Table ID here
     # must then be unique for every class/table.
-    vmdef.set_object_id(@agent.alloc_object_id(ar_vm.id, VmDef::TABLE_ID))
+    vmdef.set_object_id(encode_id(ar_vm.id))
 
     return vmdef
   end
 
-  def implement_id_query(context, id)
-    vmsvc = VmQmfController.new
-    vmsvc.svc_show(id)
-    vmdef = ar_to_object(vmsvc.vm)
-    @agent.query_response(context, vmdef) if vmdef
+  def find(id)
+    svc_show(id)
+    ar_to_object(@vm)
   end
 
-  def implement_class_query(context)
-    vmsvc = VmQmfController.new
-
+  def list
     puts "query for VmDef class!"
-    # Return all VmDef objects.
+    # Return all VmDef objects. FIXME: Use VmService
     ar_vms = Vm.find(:all)
-    ar_vms.each do |ar_vm|
-      begin
-        vmsvc.svc_show(ar_vm.id)
-        vmdef = ar_to_object(vmsvc.vm)
-        @agent.query_response(context, vmdef)
-      rescue Exception => ex
-        @logger.info "Couldn't get svc_show to show vm record: #{ex}"
-      end
-    end
-  end
-
-  def implement_method_call(context, method)
+    ar_vms.collect { |ar_vm| ar_to_object(ar_vm) }
   end
 
 end
 
 class OvirtAgent < Qmf::AgentHandler
 
+  include Ovirt::SchemaParser
+
   def initialize(host)
 
     ensure_credentials
 
     @logger = Logger.new(STDERR)
+    @logger.level = Logger::DEBUG
 
     server, port = nil
     sleepy = 5
@@ -200,62 +220,64 @@ class OvirtAgent < Qmf::AgentHandler
     @connection = Qmf::Connection.new(@settings)
     @agent = Qmf::Agent.new(self)
 
-    @ovirt_model = Ovirt.new(@agent, @logger)
-    @vmdef_model = VmDef.new(@agent, @logger)
+    @schema_classes = schema_parse(SCHEMA_XML)
 
     # Create a hash which maps "table ID"s to specific models.  This
     # number is then encoded into the local part of the object ID so
     # we always know what object type and database row the object id
     # refers to.
-    @table_id_map = { Ovirt::TABLE_ID => @ovirt_model,
-                      VmDef::TABLE_ID => @vmdef_model
-                    }
+    @controller_classes = @schema_classes.inject({}) do |map, klass|
+      begin
+        controller_class = "#{klass.name}Controller".constantize
+        raise NameError unless controller_class < AgentController
+        map[klass.id] = controller_class
+        controller_class.schema_class = klass
+      rescue NameError
+        @logger.info "No controller for #{klass.name}"
+      end
+      map
+    end
+
+    @controller_classes.values.each do |controller_class|
+      @logger.info "Register #{controller_class.schema_class.name} => #{controller_class.name}"
+      @agent.register_class(controller_class.schema_class)
+    end
+
   end
 
   # This method is called when a console does a search for a specific
   # object.  It should use query_response() to return the matching objects
   # and then query_complete() when done.
   def get_query(context, query, user_id)
-
+    @logger.error "********** get_query"
     begin
-      puts "Query: context=#{context} class=#{query.class_name} object_id=#{query.object_id}"
-      puts "User ID: #{user_id}"
+      @logger.debug "Query: context=#{context} class=#{query.class_name} object_id=#{query.object_id}"
+      @logger.debug "User ID: #{user_id}"
 
-      if query.object_id != nil
-        object_num_low = query.object_id.object_num_low
-        object_num_high = query.object_id.object_num_high
+      if query.object_id
+        # Lookup individual object
+        controller_id, row_id = @agent.decode_id(object_id)
 
-        puts "Query: object_num=#{object_num_low},#{object_num_high}"
+        @logger.debug "Query: object_num=#{controller_id}:#{row_id}"
 
-        # We use object_num_high (32 bit int) to denote the table or class type,
-        # and object_num_low (32 bits also) to denote the row ID in the database.
-        begin
-          cls = @table_id_map[object_num_high]
-          cls.implement_id_query(context, object_num_low)
-          @agent.query_complete(context)
-          return
-        rescue Exception => ex
-          @logger.info "Couldn't map ID to object: #{ex}"
-          @logger.info ex.backtrace
+        controller = controller_for_id(controller_id)
+        assert_controller_responds(controller, :find)
+        if obj = controller.find(row_id)
+          @agent.query_response(context, obj)
+        else
+          raise "No Object found for #{controller.class.name}:#{row_id}"
         end
-
-        @agent.query_complete(context)
-        return
-      end
-
-      @table_id_map.each do |id, cls|
-        begin
-          puts "checking against class #{cls.class.to_s}"
-          if query.class_name == cls.class.to_s
-            cls.implement_class_query(context)
-            puts "query complete..."
-            @agent.query_complete(context)
-            puts "yes"
-            return
+      else
+        # Class query
+        controller_class = @controller_classes.values.find { |klass|
+          klass.name == query.class_name
+        }
+        controller = controller_instance(context, controller_class)
+        assert_controller_responds(controller, :list)
+        if objs = controller.list
+          objs.each do |obj|
+            @agent.query_response(context, obj)
           end
-        rescue Exception => ex
-          @logger.error "Error performing class name query ovirt-agent: #{ex}"
-          @logger.error ex.backtrace
         end
       end
 
@@ -264,29 +286,48 @@ class OvirtAgent < Qmf::AgentHandler
       @logger.error ex.backtrace
     end
 
-    puts "empty return!"
+    # FIXME: How do you properly report errors for queries ?
     @agent.query_complete(context)
-
   end
-
 
   # This is called when an incoming method is requested of an object.
   def method_call(context, name, object_id, args, user_id)
     begin
-      object_num_low = object_id.object_num_low
-      object_num_high = object_id.object_num_high
+      controller_id, row_id = @agent.decode_id(object_id)
 
-      puts "Method: context=#{context} method=#{name} object_num=#{object_num_low},#{object_num_high} args=#{args}"
-      puts "User ID: #{user_id}"
-      cls = @table_id_map[object_num_high]
-      cls.implement_method_call(context, name, object_num_low, args)
+      @logger.debug "Method: context=#{context} method=#{name} row_id=#{row_id}, args=#{args}"
+      @logger.debug "User ID: #{user_id}"
+
+      controller = controller_for_id(context, controller_id)
+      assert_controller_responds(controller, name)
+
+      controller.args = args
+      controller.send(name)
+
+      @agent.method_response(context, 0, "OK", args)
     rescue Exception => ex
-      @logger.info "Error implementing method in ovirt-agent: #{ex}"
+      @logger.info "Error calling #{name}: #{ex}"
       @logger.info ex.backtrace
       @agent.method_response(context, 1, "ERROR: #{ex}", args)
     end
   end
 
+  def controller_for_id(context, id)
+    unless controller_class = @controller_classes[id]
+      raise ControllerNotFoundError, "unknown controller id #{controller_id}"
+    end
+    controller_instance(context, controller_class)
+  end
+
+  def controller_instance(context, controller_class)
+    controller_class.new(context, @agent, @logger)
+  end
+
+  def assert_controller_responds(controller, method)
+    unless controller.respond_to?(name)
+      raise ArgumentError, "unknown method #{name} for #{controller.class.name}"
+    end
+  end
 
   def ensure_credentials()
     get_credentials('qpidd')
@@ -303,9 +344,9 @@ class OvirtAgent < Qmf::AgentHandler
     Thread.abort_on_exception = true
 
     @agent.set_connection(@connection)
-
-    @ovirt_model.start
-
+    @controller_classes.values.each do |klass|
+      klass.setup(@agent) if klass.respond_to?(:setup)
+    end
     sleep
   end
 end
