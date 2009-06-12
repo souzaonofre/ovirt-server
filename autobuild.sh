df042adf4f02c2f4418691fadd21e494ad3b65af
Mohammed Morsi <mmorsi@redhat.com>
2009-06-12 13:49:51 -0400

    ovirt server installer autobuild integration
    update server autobuild to set up a new isolated vm environment and
      go through the server installer process and rails tests

--- autobuild.sh
+++ autobuild.sh
@@ -25,10 +25,13 @@ set -v
 
 test -f Makefile && make -k distclean || :
 
+############# build the server rpm
+
 # put rails in development mode
 cp conf/ovirt-rails.sysconf conf/ovirt-rails.sysconf.orig
 echo "RAILS_ENV=development" >> conf/ovirt-rails.sysconf
 
+# build server & installer rpms
 ./autogen.sh --prefix=$AUTOBUILD_INSTALL_ROOT
 make dist
 
@@ -43,4 +46,73 @@ if [ -f /usr/bin/rpmbuild ]; then
   rpmbuild --nodeps --define "extra_release $EXTRA_RELEASE" -ta --clean *.tar.gz
 fi
 
+# restore to checkout
 mv conf/ovirt-rails.sysconf.orig conf/ovirt-rails.sysconf
+
+############## setup new vm to test installer
+
+# setup parameters to ssh to vm
+SSHKEY=~/.ssh/id_autobuild
+remote_target="root@192.168.122.190" # should be an address on default libvirt network
+ssh_cmd="ssh -i $SSHKEY -o StrictHostKeyChecking=no \
+             -o UserKnownHostsFile=/dev/null $remote_target"
+scp_cmd="scp -i $SSHKEY -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+
+# generate new ssh key if not found
+if [ ! -r $SSHKEY ]; then
+  mkdir -p $(dirname "$SSHKEY")
+  ssh-keygen -q -t rsa -N "" -f $SSHKEY
+fi
+
+# copy the key and installer answers to the kickstart
+cp ovirt-server-test.ks ovirt-server-test.ks.orig
+cat >> ovirt-server-test.ks << KS
+%post
+mkdir -p /root/.ssh
+chmod 700 /root/.ssh
+cat > /root/.ssh/authorized_keys << \EOF
+$(ssh-keygen -y -f $SSHKEY)
+EOF
+chmod 600 /root/.ssh/authorized_keys
+
+cat > /root/ovirt-installer-answers << \EOF
+$(cat ovirt-installer-test-answers)
+EOF
+
+%end
+KS
+
+# remove old server vm
+test ! -f ovirt-server-test.tar || rm -f ovirt-server-test.tar
+test ! -d test-vm || rm -rf test-vm
+sudo virsh destroy ovirt-server-test || true
+sudo virsh undefine ovirt-server-test || true
+
+# create new vm for server, configuring via kickstart, boot it
+sudo appliance-creator --config ovirt-server-test.ks --name ovirt-server-test \
+                       -f qcow2 -p tar -d -v
+
+# restore original kickstart
+mv ovirt-server-test.ks.orig ovirt-server-test.ks
+
+# define the libvirt vm, boot it
+mkdir -p test-vm
+tar -xSvf ovirt-server-test.tar -C test-vm
+sudo virt-image test-vm/ovirt-server-test/ovirt-server-test.xml
+sudo virsh define /etc/libvirt/qemu/ovirt-server-test.xml
+
+################### test server vm
+
+# wait till ovirt-appliance is started
+for i in $(seq 1 60); do
+  $ssh_cmd exit && break
+  sleep 10
+done
+
+# run installer on server vm, answering preconfigured questions
+$ssh_cmd "ovirt-installer < /root/ovirt-installer-answers"
+$ssh_cmd "ace -d install ovirt" || true  # FIXME when "installer always returns failed"
+                                         #  bug is fixed, remove this "|| true"
+
+# run tests on newly installed server
+$ssh_cmd "cd /usr/share/ovirt-server && rake db:migrate && rake test"
