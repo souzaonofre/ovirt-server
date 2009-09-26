00212c93c1f717e95fa7c56ea859d930a1f90425
Michel Loiseleur <mloiseleur@linagora.com>
2009-09-26 17:25:52 +0200

    fixes rails launch when a rack gem is present on the system
    Signed-off-by: Michel Loiseleur <mloiseleur@linagora.com>

--- src/config/initializers/mime_types.rb
+++ src/config/initializers/mime_types.rb
@@ -3,3 +3,9 @@
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 # Mime::Type.register_alias "text/html", :iphone
+
+# Workaround for rack gem + rails gem 2.3.2 on a same machine
+# See https://rails.lighthouseapp.com/projects/8994/tickets/2784-private-method-split-called-for-mimetype0x226f618
+class Mime::Type
+  delegate :split, :to => :to_s
+end
