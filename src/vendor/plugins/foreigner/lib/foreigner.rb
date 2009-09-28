4c6779532cf6e0c6b8e3c7f9131bcd183f1570da
Michel Loiseleur <mloiseleur@linagora.com>
2009-09-28 22:08:30 +0200

    Add foreigner plugin from Matthu Higgins, allowing to manage foreign key in a multi-db way.
    Signed-off-by: Michel Loiseleur <mloiseleur@linagora.com>

---
+++ src/vendor/plugins/foreigner/lib/foreigner.rb
@@ -0,0 +1,21 @@
+require 'foreigner/connection_adapters/abstract/schema_statements'
+require 'foreigner/connection_adapters/abstract/schema_definitions'
+require 'foreigner/connection_adapters/sql_2003'
+require 'foreigner/schema_dumper'
+
+module ActiveRecord
+  module ConnectionAdapters
+    include Foreigner::ConnectionAdapters::SchemaStatements
+    include Foreigner::ConnectionAdapters::SchemaDefinitions
+  end
+
+  SchemaDumper.class_eval do
+    include Foreigner::SchemaDumper
+  end
+
+  Base.class_eval do
+    if %w(MySQL PostgreSQL).include? connection.adapter_name
+      require "foreigner/connection_adapters/#{connection.adapter_name.downcase}_adapter"
+    end
+  end
+end
---
+++ src/vendor/plugins/foreigner/lib/foreigner/connection_adapters/mysql_adapter.rb
@@ -0,0 +1,43 @@
+module Foreigner
+  module ConnectionAdapters
+    module MysqlAdapter
+      include Foreigner::ConnectionAdapters::Sql2003
+
+      def foreign_keys(table_name)
+        fk_info = select_all %{
+          SELECT fk.referenced_table_name as 'to_table'
+                ,fk.referenced_column_name as 'primary_key'
+                ,fk.column_name as 'column'
+                ,fk.constraint_name as 'name'
+          FROM information_schema.key_column_usage fk
+          WHERE fk.referenced_column_name is not null
+            AND fk.table_schema = '#{@config[:database]}'
+            AND fk.table_name = '#{table_name}'
+        }
+
+        create_table_info = select_one("SHOW CREATE TABLE #{quote_table_name(table_name)}")["Create Table"]
+
+        fk_info.map do |row|
+          options = {:column => row['column'], :name => row['name'], :primary_key => row['primary_key']}
+
+          if create_table_info =~ /CONSTRAINT #{quote_column_name(row['name'])} FOREIGN KEY .* REFERENCES .* ON DELETE (CASCADE|SET NULL)/
+            if $1 == 'CASCADE'
+              options[:dependent] = :delete
+            elsif $1 == 'SET NULL'
+              options[:dependent] = :nullify
+            end
+          end
+          ForeignKeyDefinition.new(table_name, row['to_table'], options)
+        end
+      end
+    end
+  end
+end
+
+module ActiveRecord
+  module ConnectionAdapters
+    MysqlAdapter.class_eval do
+      include Foreigner::ConnectionAdapters::MysqlAdapter
+    end
+  end
+end
